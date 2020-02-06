725f2494642d27c96fe0d24c51461d592d8153b9
1795186 <salva.mtz13@hotmail.com>
2020-02-06 13:57:09 -0600

    Actualizar

--- Script_1.R
+++ Script_1.R
@@ -1,10 +1,12 @@
  library(repmis)
  conjunto <- source_data("https://www.dropbox.com/s/hmsf07bbayxv6m3/cuadro1.csv?dl=1")
 
+
 # media -------------------------------------------------------------------
 
 
  mean(conjunto$Altura)
+ 
  
 
 # altura ------------------------------------------------------------------
@@ -56,6 +58,10 @@ Altura_cedro <- subset(cedro_rojo,
  
  Altura_douglasia <- subset(Douglasia_verde, Douglasia_verde$Altura > 18.5)
  
+ HyF <- rbind(Tsuga_heterofilia, Douglasia_verde)
+ tsu.doug <- subset(conjunto, conjunto$Especie == "H" | conjunto$Especie =="F")
+ 
+ 
  # histograma --------------------------------------------------------------
 
 hist(conjunto$Altura, col= "red", xlab = "Altura (m)",
@@ -66,16 +72,60 @@ hist(conjunto$Altura, col= "red", xlab = "Altura (m)",
  
  hist(conjunto$Diametro, col= "yellow", xlab = "Diametro",
       ylab = "Frecuencia", main = "Histograma de Diametro")
+ 
 
+# 3 graficas --------------------------------------------------------------
+
+par(mfrow=c(2,2) )
+hist(Tsuga_heterofilia$Altura, main = "Altura especie H" , col = "red" ,
+     xlab = "Altura" , ylab = "Frecencia") 
+hist(Douglasia_verde$Altura, main = "Altura especie F" , col = "blue",
+     xlab = "Altura" , ylab = "Frecuencia") 
+hist(Tsuga_heterofilia$Diametro, main = "Diametro especie H" , col = "yellow" ,
+     xlab = "Diametro" , ylab = "Frecuencia")
+hist(Douglasia_verde$Diametro, main = "Diametro especie F" , col = "green" ,
+     xlab = "Diametro" , ylab = "Frecuencia")
+par(mfrow=c(1,1))
+
+par(mfrow=c(2,2))
+hist(H.media$Altura, col = "red" , xlab = "Alturas iguales o menores a la media", ylab = "Frecuencia")
+hist(H.16$Altura, col = "blue", xlab = "Alturas mayores a 16", ylab = "Frecuencia")
+hist(vecinos_3$Vecinos, col = "black", xlab = "Vecinos iguales o mayores a 3", ylab = "Frecuencia")
+hist(vecinos_4$Vecinos, col = "green", xlab = "Vecinos mayores a 4", ylab = "Frecuencia")
+hist(DBH_media$Diametro,  col = "Orange", xlab = "Diametros mayores a la media", ylab = "Frecuencia")
+hist(DBH_16$Diametro,  col = "yellow", xlab = "Diametros mayores a 16", ylab = "Frecuencia")
+par(mflow=c(1,1))
 
 # basicos -----------------------------------------------------------------
 
 mean(conjunto$Altura)  
-sd(conjunto$Altura)
-  
- 
 mean(conjunto$Vecinos)  
-sd(conjunto$Vecinos)
+mean(conjunto$Diametro) 
+mean(H.media$Altura)
+mean(H.16$Altura)
+mean(vecinos_3$Vecinos)
+mean(vecinos_4$Vecinos)
+mean(DBH_media$Diametro)
+mean(DBH_16$Diametro)
 
-mean(conjunto$Diametro)  
+
+sd(conjunto$Altura)
+sd(conjunto$Vecinos)
 sd(conjunto$Diametro)
+sd(H.media$Altura)
+sd(H.16$Altura)
+sd(vecinos_3$Vecinos)
+sd(vecinos_4$Vecinos)
+sd(DBH_media$Diametro)
+sd(DBH_16$Diametro)
+
+
+
+
+
+
+
+
+
+
+
